3c883d65b856c7f38d116bf6fe89bc46871a7a32
Lucas Gomes <lukmck@gmail.com>
2024-08-12 22:32:10 -0300

    Adicionando arquivo de configurações do jogo

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """Uma clas para armazenar todas as configurações da Invasão Alienigena"""
+
+    def __init__(self):
+        """Inicializa as configurações do jogo"""
+        # Configurações de tela
+        self.screen_width = 900
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
