32aa2ecb15454135f987e43fa458eaa0ac2c5b63
Lucas Gomes <lukmck@gmail.com>
2024-08-12 22:33:53 -0300

    alterando arquivo principal com a classe do arquivo settings

--- alien_invasor.py
+++ alien_invasor.py
@@ -1,20 +1,25 @@
 import sys
 import pygame
+from settings import Settings
 
 def run_game():
     # Inicializa o jogo e cria um objeto para a tela
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption("Alien Invasion")
-    # Inicia o laço principal do jogo
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion") 
 
+    # Inicia o laço principal do jogo
     while True:
         # Observa eventos de teclado e de mouse
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-                # Deixa a tela mais recente visível
-                pygame.display.flip()
+
+            # Redesenha a tela a cada passagem do laço
+            screen.fill(ai_settings.bg_color)
+            # Deixa a tela mais recente visível
+            pygame.display.flip()
 
                 
 run_game()
