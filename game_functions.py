5b9efeb43fe8d675ae277b00717c684dc0a532c8
Lucas Gomes <lukmck@gmail.com>
2024-08-14 22:14:00 -0300

    adicionando movimento a nave

--- game_functions.py
+++ game_functions.py
@@ -1,10 +1,20 @@
 import sys, pygame
 
-def check_events():
+def check_events(ship):
     '''Responde a eventos de presionamento de teclas e de mouse'''
     for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 
 def update_screen(ai_settings, screen, ship):
