bf9f3f9f4207d2dfb50971d571d06ca19800b1b3
Lucas Gomes <lucasfgsantos01@gmail.com>
2025-05-13 08:26:48 -0300

    adicionando alguns comentarios, corrigindo bug de tiro

--- alien_invasor.py
+++ alien_invasor.py
@@ -1,19 +1,22 @@
 import pygame
-import game_functions as gf
-from settings import Settings
-from ship import Ship
 from pygame.sprite import Group
 
+from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
-    # Inicializa o jogo e cria um objeto para a tela
+    # Inicializa o pygame, as configurações e o objeto screen
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    ship = Ship(ai_settings, screen) # Cria uma nave
-    bullets = Group() # Cria um grupo no qual serão armazenados os projéteis
 
+    # Cria uma espaçonave
+    ship = Ship(ai_settings, screen)
+
+    # Cria um grupo no qual serão armazenados os projéteis
+    bullets = Group()
 
     # Inicia o laço principal do jogo
     while True:
@@ -22,5 +25,5 @@ def run_game():
         bullets.update()
         gf.update_screen(ai_settings, screen, ship, bullets)
 
-                
+
 run_game()
